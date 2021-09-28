18660fbf0cb1e599443f43a717f9e01c49772219
riveriswild <79055927+riveriswild@users.noreply.github.com>
2021-09-28 14:13:44 +0300

    Add files via upload

--- 5.js
+++ 5.js
@@ -1,67 +1,70 @@
 let allVoltage = new Array();
 
 class ElectricalDevice {
-    constructor(name, voltage){
-        this.name = name,
-        this.plugged = false,
-        this.voltage = voltage
-    }
-    switchOn(allVoltage) {
-        this.plugged = true,
-        allVoltage.push(this),
-        console.log(`The ${this.name} was turned on. Current voltage: ${this.voltage}`);
-    }
-    switchOff(allVoltage) {
-        this.plugged = false,
-        removeFromVoltage(allVoltage, this);
-        console.log(`The ${this.name} was turned off.`);
-    };
+  constructor(options) {
+    this.name = options.name,
+    this.plugged = false,
+    this.voltage = options.voltage
+  }
 
-    removeFromVoltage(arr, value) {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === value) {
-              arr.splice(i, 1);
-            }
-          }
+  switchOn(allVoltage) {
+    this.plugged = true,
+    allVoltage.push(this),
+    console.log(`The ${this.name} was turned on. Current voltage: ${this.voltage}`);
+  }
+
+  removeFromVoltage(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        arr.splice(i, 1);
+      }
     }
+  }
+
+  switchOff(allVoltage) {
+    this.plugged = false,
+    this.removeFromVoltage(allVoltage, this);
+    console.log(`The ${this.name} was turned off.`);
+  };
 }
 
 class LampDevice extends ElectricalDevice {
-    constructor(name, voltage, brightness, colour, plugged){
-        super(plugged);
-        this.name = name,
-        this.voltage = voltage,
-        this.brightness = brightness,
-        this.colour = colour
-    }
-    switchOn(allVoltage) {
-        this.plugged = true,
-        allVoltage.push(this),
-        console.log(`The ${this.name} was turned on. Current voltage: ${this.voltage}, brightness: ${this.brightness}, colour: ${this.colour}`);
-    }
+  constructor(options) {
+    super(options);
+    this.name = options.name,
+    this.voltage = options.voltage,
+    this.brightness = options.brightness,
+    this.colour = options.colour
+  }
+
+  switchOn(allVoltage) {
+    this.plugged = true,
+    allVoltage.push(this),
+    console.log(`The ${this.name} was turned on. Current voltage: ${this.voltage}, brightness: ${this.brightness}, colour: ${this.colour}`);
+  }
 }
 
 function allPower(allVoltage) {
-    let consumed = 0;
-    allVoltage.forEach((i) => {
-      if (i.plugged) {
-        consumed = consumed + i.voltage;
-      }
-    });
-    return consumed;
-  }
+  let consumed = 0;
+  allVoltage.forEach((i) => {
+    if (i.plugged) {
+      consumed = consumed + i.voltage;
+    }
+  });
+  return consumed;
+}
 
-  const lamp = new LampDevice('lamp', 25, 50, 'red');
-  const PC = new ElectricalDevice('pc', 150);
-  const fridge = new ElectricalDevice('fridge', 300);
-  const tableLamp = new LampDevice('table lamp', 45, 100, 'white');
-    
-  lamp.switchOn(allVoltage);
-  PC.switchOn(allVoltage);
-  console.log(`General consumption: ${allPower(allVoltage)}`);
-  fridge.switchOn(allVoltage);
-  console.log(`General consumption: ${allPower(allVoltage)}`);
-  fridge.switchOff(allVoltage);
-  console.log(`General consumption: ${allPower(allVoltage)}`);
-  tableLamp.switchOn(allVoltage);
-  console.log(`General consumption: ${allPower(allVoltage)}`);
+const lamp = new LampDevice('lamp', 25, 50, 'red');
+const PC = new ElectricalDevice('pc', 150);
+const fridge = new ElectricalDevice('fridge', 300);
+const tableLamp = new LampDevice('table lamp', 45, 100, 'white');
+
+lamp.switchOn(allVoltage);
+PC.switchOn(allVoltage);
+console.log(`General consumption: ${allPower(allVoltage)}`);
+fridge.switchOn(allVoltage);
+console.log(`General consumption: ${allPower(allVoltage)}`);
+fridge.switchOff(allVoltage);
+console.log(`General consumption: ${allPower(allVoltage)}`);
+tableLamp.switchOn(allVoltage);
+console.log(`General consumption: ${allPower(allVoltage)}`);
